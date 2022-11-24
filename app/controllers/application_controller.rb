3c38910703f6a05c5b615cd88a868fb53e46b38e
LaveeshkaMahadea <laveeshka94@gmail.com>
2022-11-25 00:59:33 +1100

    complete lab

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,30 @@
 class ApplicationController < Sinatra::Base
 
+  #set the default content type as json
+  set :default_content_type, 'application/json'
+
   # add routes
+  get '/bakeries' do
+    # get all the bakeries from the database
+    bakeries = Bakery.all
+    # send them back as a JSON array
+    bakeries.to_json
+  end
+
+  get '/bakeries/:id' do
+    bakery = Bakery.find(params[:id])
+    bakery.to_json(include: :baked_goods)
+  end
+
+  get '/baked_goods/by_price' do
+    baked_goods = BakedGood.all.order(price: :desc)
+    baked_goods.to_json
+  end
+
+  get '/baked_goods/most_expensive' do
+    highest_price = BakedGood.maximum(:price)
+    most_expensive_baked_good = BakedGood.find_by(price: highest_price)
+    most_expensive_baked_good.to_json
+  end
 
 end
